811caf6d0d28d92db052d1ea744d68fdf62f04d1
wyf-ACCEPT <define_wifi@163.com>
2024-08-19 08:53:18 +0000

    save

--- scripts/baku-parse.js
+++ scripts/baku-parse.js
@@ -1,5 +1,5 @@
+const winston = require('winston')
 const { getFullnodeUrl, SuiClient } = require('@mysten/sui/client')
-const { writeFileSync } = require('fs')
 require('dotenv').config()
 
 const logger = winston.createLogger({
@@ -18,12 +18,15 @@ const logger = winston.createLogger({
 })
 
 const main = async () => {
-  const rpcUrl = process.env.BAKU_RPC
+  const rpcUrl = process.env.RPC_BAKU
   const client = new SuiClient({ url: rpcUrl })
-  const { cp: cpStart, txns: txnsStart } = await findCheckpoint("2024-08-06T00:00:00Z", client)
-  const { cp: cpEnd, txns: txnsEnd } = await findCheckpoint("2024-08-12T23:59:59Z", client)
-  console.log(`\n${INFO} Checkpoint range: ${cpStart} - ${cpEnd} (total ${cpEnd - cpStart} checkpoints)`)
-  console.log(`${INFO} Transaction range: ${txnsStart} - ${txnsEnd} (total ${txnsEnd - txnsStart} transactions)`)
+  console.log(await client.getLatestCheckpointSequenceNumber())
+  console.log(await client.getTotalTransactionBlocks())
+  console.log(await client.getCheckpoint({ id: "9430000" }))
+  // const { cp: cpStart, txns: txnsStart } = await findCheckpoint("2024-08-06T00:00:00Z", client)
+  // const { cp: cpEnd, txns: txnsEnd } = await findCheckpoint("2024-08-12T23:59:59Z", client)
+  // console.log(`\n${INFO} Checkpoint range: ${cpStart} - ${cpEnd} (total ${cpEnd - cpStart} checkpoints)`)
+  // console.log(`${INFO} Transaction range: ${txnsStart} - ${txnsEnd} (total ${txnsEnd - txnsStart} transactions)`)
 }
 
 main()
