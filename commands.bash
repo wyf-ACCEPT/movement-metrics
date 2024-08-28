60739970d183734cbd4f2e98f776714ed38d9dfd
wyf-ACCEPT <define_wifi@163.com>
2024-08-28 02:52:30 +0000

    modify bash prompt

--- commands.bash
+++ commands.bash
@@ -4,9 +4,13 @@ grep -o "succeeded" ./logs/imola-send.log | wc -l; grep -o "ApiError" ./logs/imo
 # [Parse] How about the progress?
 head -n 3 ./logs/imola-send.log
 tail -n 3 ./logs/imola-send.log
-watch -n 1 tail -n 25 ./logs/baku-parsing.log
+watch -n 1 tail -n 25 ./logs/baku-online.log
 watch -n 1 tail -n 25 ./logs/imola-online.log
 
 # [Parse] Check rows?
 cat ./logs/baku-parsing.log | wc -l
 cat ./logs/imola-parsing.log | wc -l
+
+# [Parse] Online parse
+nohup node ./scripts/online-baku.js &
+nohup node ./scripts/online-imola.js &
